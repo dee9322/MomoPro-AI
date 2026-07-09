8df1d168764b0f60256ead5c9a7676a224046043
dee9322 <dbardwell9322@gmail.com>
2026-07-09 01:35:37 -0700

    Add list of stock symbols to symbols.py

---
+++ symbols.py
@@ -0,0 +1,13 @@
+SYMBOLS = [
+    "AAPL", "MSFT", "NVDA", "AMD", "TSLA", "META", "GOOGL", "AMZN", "NFLX",
+    "PLTR", "SOFI", "HOOD", "COIN", "MARA", "RIOT", "RGTI", "IONQ", "SOUN",
+    "RKLB", "OPEN", "AFRM", "UPST", "DKNG", "RBLX", "SNAP", "SHOP", "SQ",
+    "PYPL", "UBER", "LYFT", "ROKU", "LCID", "RIVN", "NIO", "XPEV", "LI",
+    "F", "GM", "CCL", "NCLH", "DAL", "UAL", "AAL", "BA", "DIS", "WBD",
+    "PARA", "T", "VZ", "INTC", "MU", "SMCI", "ARM", "AVGO", "QCOM",
+    "MRVL", "TSM", "BABA", "PDD", "JD", "BIDU", "GME", "AMC", "CVNA",
+    "CHWY", "AI", "PATH", "LMND", "U", "NET", "DDOG", "SNOW", "CRWD",
+    "ZS", "MDB", "PANW", "OKTA", "ENPH", "SEDG", "FSLR", "RUN", "ARRY",
+    "BTBT", "CLSK", "HUT", "WULF", "IREN", "BITF", "JOBY", "ACHR", "SPCE",
+    "DNA", "BBAI", "QBTS", "LAZR", "OUST", "ENVX", "QS", "CHPT", "BLNK"
+]
